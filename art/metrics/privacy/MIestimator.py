distance calculation in joint space bug fix

art/metrics/privacy/MIestimator.py
@@ -43,18 +43,22 @@ class MI:
         return [sum(sublist, []) for sublist in zip(*args)]
 
     @staticmethod  ## WE CHANGED ORIGINAL FUNCTION!!! NOW P IS A PARAMETER
-    def avgdigamma(points, dvec, p=float(
-        'inf')):  ## calculates the avg over N, in a specific dimension i (1 <= i <= d)! see formula 15
+    def avgdigamma(points, dvec, p=float('inf')):
+        ## calculates the avg over N, in a specific dimension i (1 <= i <= d)! see formula 15
         # This part finds number of neighbors in some radius in the marginal space
         # returns expectation value of <psi(nx)>
         N = len(points)
-        tree = ss.cKDTree(points)
+        if p != float('inf'):
+            metric = neighbors.DistanceMetric.get_metric('minkowski', p=p)
+        else:
+            metric = neighbors.DistanceMetric.get_metric('chebyshev')
+        tree = neighbors.BallTree(points, metric=metric)
         avg = 0.
         for i in range(N):
             dist = dvec[i]
             # subtlety, we don't include the boundary point,
             # but we are implicitly adding 1 to kraskov def bc center point is included
-            num_points = len(tree.query_ball_point(points[i], dist - 1e-15, p=p))
+            num_points = tree.query_radius([points[i]], dist - 1e-15, count_only=True)[0]
             avg += digamma(num_points) / N
         return avg
 
@@ -65,14 +69,15 @@ class MI:
         '''
         p_x = kwargs["metric_params"]["p_x"]
         p_y = kwargs["metric_params"]["p_y"]
-        x1 = z1[:-1]
-        x2 = z2[:-1]
+        size_of_y = kwargs["metric_params"]["size_of_y"]
+        x1 = z1[:-size_of_y]
+        x2 = z2[:-size_of_y]
         if p_x != float('inf'):
             x_dist = ss.distance.minkowski(x1, x2, p=p_x)
         else:
             x_dist = ss.distance.chebyshev(x1, x2)
-        y1 = z1[-1]
-        y2 = z2[-1]
+        y1 = z1[-size_of_y:]
+        y2 = z2[-size_of_y:]
         if p_y != float('inf'):
             y_dist = ss.distance.minkowski(y1, y2, p=p_y)
         else:
@@ -108,7 +113,7 @@ class MI:
             joint_space_metric = neighbors.DistanceMetric.get_metric('chebyshev')
         else:
             joint_space_metric = neighbors.DistanceMetric.get_metric('pyfunc', func=MI.__joint_space_dist,
-                                                                     metric_params={"p_x": p_x, "p_y": p_y})
+                                                                     metric_params={"p_x": p_x, "p_y": p_y, "size_of_y":Y.shape[1]})
         tree = neighbors.BallTree(points, metric=joint_space_metric)
         d_vec = np.zeros((2, N)) - 1
         ''' Denote the j'th sample in the marginal space Z=(X,Y) by (x_j, y_j).
@@ -120,6 +125,7 @@ class MI:
         for point in points:
             sample += 1
             distances, indices = tree.query([point], k + 1)
+            #print("we are here")
             distances = distances[
                 0]  ## just for fixing the shape... see sklearn.neighbors.BallTree documantation for query return value if not clear
             indices = indices[0]
@@ -130,7 +136,7 @@ class MI:
                 if d_vec[1][sample] < np.max(np.fabs(points[sample][-Y.shape[1]:] - neighbor[-Y.shape[1]:])):
                     d_vec[1][sample] = np.max(np.fabs(points[sample][-Y.shape[1]:] - neighbor[-Y.shape[1]:]))
 
-        avg_digamma = MI.avgdigamma(X, d_vec[0], p_x) + MI.avgdigamma(Y, d_vec[1], p_y)
+        avg_digamma = MI.avgdigamma(X, d_vec[0], p=p_x) + MI.avgdigamma(Y, d_vec[1], p=p_y)
         return digamma(N) + digamma(k) - 1 / k - avg_digamma
 
     @staticmethod

tests/attacks/inference/membership_inference/test_mi_metric_with_black_box.py
@@ -10,13 +10,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn import preprocessing
 from sklearn.metrics import accuracy_score
+from scipy.stats import binom
 from matplotlib import pyplot as plt
 from math import comb, log
 
 
 ## p_aplpha >= lower bound. p_alpha is the probability of MIA model making more then alpha prediction errors
 def calc_LB_prob_MIA_errors(training_data_D, output_Y, size_of_data_set, Alphas, k, p):
-    mi_D_Y = mi.mi_Kraskov_HnM(training_data_D, output_Y, k=k, p_x=p)
+    mi_D_Y = mi.mi_Kraskov_HnM(training_data_D, output_Y, k=k, p_x=p, p_y=p)                # p_y =p ?????????????????????????????????????????????/
     Print(f'\t\tMI(D,Y)={mi_D_Y}, k={k}, p={p}')
     entropy_D_orig = mi.entropy(training_data_D, k=k)
     entropy_D_p = mi.entropy(training_data_D, k=k, p=p)
@@ -76,9 +77,10 @@ def plot_mi_as_func_of_of_k(x_train, x_test, K, P, model_name, data_set_name):
 
 # calculating the probability of making at least alpha prediction mistakes given the (empirical) probability of making one mistake (assuming independence- binomial dist.)
 def calc_prob_of_alpha_mistakes(mistake_prob, alpha, size_of_data_set):
-    return sum(
-        [comb(size_of_data_set, i) * (mistake_prob ** i) * ((1 - mistake_prob) ** (size_of_data_set - i)) for i in
-         range(alpha, size_of_data_set + 1)])
+    # return sum(
+    #    [comb(size_of_data_set, i) * (mistake_prob ** i) * ((1 - mistake_prob) ** (size_of_data_set - i)) for i in
+    #     range(alpha, size_of_data_set + 1)])
+    return sum([binom.pmf(i, size_of_data_set, mistake_prob) for i in range(alpha, size_of_data_set + 1)])
 
 
 # alpha is the number of mistakes that attack makes
@@ -219,8 +221,8 @@ def analyze(x_train, x_test, y_pred_train, y_pred_test, accuracy_train, accuracy
         LB_p.append([])
         LB_p_wc.append([])
         for k in K:
-            mi_train[-1].append(mi.mi_Kraskov_HnM(x_train, y_pred_train, k=k, p_x=p))
-            mi_test[-1].append(mi.mi_Kraskov_HnM(x_test, y_pred_test, k=k, p_x=p))
+            mi_train[-1].append(mi.mi_Kraskov_HnM(x_train, y_pred_train, k=k, p_x=p, p_y=p))           # SHOULD WE USE P_Y=P??????????????????????????????
+            mi_test[-1].append(mi.mi_Kraskov_HnM(x_test, y_pred_test, k=k, p_x=p, p_y=p))
             lb_orig, lb_p, lb_p_wc = calc_LB_prob_MIA_errors(x_train, y_pred_train, size_of_data_set, Alphas, k=k, p=p)
             LB_orig[-1].append(lb_orig)
             LB_p[-1].append(lb_p)
@@ -273,26 +275,84 @@ def test_landsat_with_multiple_models():
     Print("testing with landsat data...")
     landsat_train = np.loadtxt('sat.trn', delimiter=',')
     landsat_test = np.loadtxt('sat.tst', delimiter=',')
+    y_train = landsat_train[:, -1]
+    y_test = landsat_test[:, -1]
     landsat_train_normalized = preprocessing.MinMaxScaler().fit_transform(landsat_train)
     landsat_test_normalized = preprocessing.MinMaxScaler().fit_transform(landsat_test)
     x_train = landsat_train_normalized[:, :-1]  # deleting labels from samples- train set
     x_test = landsat_test_normalized[:, :-1]  # deleting labels from samples- test set
-    y_train = landsat_train_normalized[:, -1]  # labels of samples- train set
-    y_test = landsat_test_normalized[:, -1]  # labels of samples- test set
+    num_of_samples = 800
+    x_train = x_train[:num_of_samples]
+    x_test = x_test[:num_of_samples]
+    y_train = y_train[:num_of_samples]
+    y_test = y_test[:num_of_samples]
+    label_encoder = preprocessing.LabelEncoder()
+    y = np.concatenate((y_train, y_test))
+    label_encoder.fit(y)
+    y_train = label_encoder.transform(y_train)
+    y_test = label_encoder.transform(y_test)
     analyze_all_models(x_train, x_test, y_train, y_test, "landsat")
+
+def test_landsat_manually():
+    Print("testing with landsat data... MANUALLY")
+    landsat_train = np.loadtxt('sat.trn', delimiter=',')
+    landsat_test = np.loadtxt('sat.tst', delimiter=',')
+    y_train = landsat_train[:, -1]
+    y_test = landsat_test[:, -1]
+    landsat_train_normalized = preprocessing.MinMaxScaler().fit_transform(landsat_train)
+    landsat_test_normalized = preprocessing.MinMaxScaler().fit_transform(landsat_test)
+    x_train = landsat_train_normalized[:, :-1]  # deleting labels from samples- train set
+    x_test = landsat_test_normalized[:, :-1]  # deleting labels from samples- test set
+    num_of_samples = 1000
+    x_train = x_train[:num_of_samples]
+    x_test = x_test[:num_of_samples]
+    y_train = y_train[:num_of_samples]
+    y_test = y_test[:num_of_samples]
+    label_encoder = preprocessing.LabelEncoder()
+    y = np.concatenate((y_train, y_test))
+    print(y.shape)
+    label_encoder.fit(y)
+    y_train = label_encoder.transform(y_train)
+    y_test = label_encoder.transform(y_test)
+
+    ##y_train = landsat_train_normalized[:, -1]  # labels of samples- train set
+    ##y_test = landsat_test_normalized[:, -1]  # labels of samples- test set
+    #analyze_all_models(x_train, x_test, y_train, y_test, "landsat")
+    clf = LogisticRegression(random_state=42, max_iter=150)
+    y_pred_train, y_pred_test, accuracy_train, accuracy_test, attack_accuracy, mistake_prob = run_bb_attack(x_train,
+                                                                                                            x_test,
+                                                                                                            y_train,
+                                                                                                            y_test,
+                                                                                                            clf)
+    Print("y_pred_train shape", y_pred_train.shape)
+    Print("y_pred_test shape", y_pred_test.shape)
+    K = [2, 3, 5, 11]
+    P = [2.0, 1.0]
+    for p in P:
+        for k in K:
+            Print(f"p={p}, k={k}, MI train: ", mi.mi_Kraskov_HnM(x_train, y_pred_train, k=k, p_x=p, p_y=p))
+            Print(f"p={p}, k={k}, MI test: ", mi.mi_Kraskov_HnM(x_test, y_pred_test, k=k, p_x=p, p_y=p))
+            Print(f"p={p}, k={k},entropy orig: ", mi.entropy(x_train, k=k))
+            Print(f"p={p}, k={k},entropy p norm: ", mi.entropy(x_train, k=k, p=p))
+            Print(f"p={p}, k={k},entropy p norm with correction: ", mi.entropy_with_correction(x_train, k=k, p=p))
+
+
+
+
 
 
 def main():
     Print("----- STARTING TESTS! ------")
-    test_german_with_multiple_models()
+    #test_german_with_multiple_models()
     test_landsat_with_multiple_models()
+    #test_landsat_manually()
     Print("----- DONE :) -----")
 
 
 def Print(string, *args):
     print(string, *args)
     print(string, *args, file=output)
-    #output.flush()
+    # output.flush()
 
 
 if __name__ == "__main__":